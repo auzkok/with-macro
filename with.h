bf9b9a7861c3d35fb5c614f33efcca1eb3987314
auzkok <35302680+auzkok@users.noreply.github.com>
2025-08-14 15:12:28 +0200

    Rename the parameters

--- with.h
+++ with.h
@@ -1,6 +1,6 @@
 #ifndef WITH_H_INCLUDED
 #define WITH_H_INCLUDED
 
-#define WITH(init, deinit) for (int w_o_ = 1, w_i_ = 1; w_o_; w_o_ = 0) for (init; w_i_; w_i_ = 0, (deinit))
+#define WITH(before, after) for (int w_o_ = 1, w_i_ = 1; w_o_; w_o_ = 0) for (before; w_i_; w_i_ = 0, (after))
 
 #endif // WITH_H_INCLUDED
